fa4bfc7bc3884e429e0c12638eaa672f3da17f56
EvelynPloetz <108936623+EvelynPloetz@users.noreply.github.com>
2023-02-11 21:53:27 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,8 +19,6 @@ extensions = [
     'sphinx.ext.intersphinx',
 ]
 
-extensions = ["sphinx_design"] #Evelyn: for drop-down boxes
-
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
